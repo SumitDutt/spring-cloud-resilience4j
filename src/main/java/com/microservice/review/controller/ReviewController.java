1f37ae241005eb19f79876ddfb31a571f8cae69f
Sumit Dutt Tripati <sumitdutt85@gmail.com>
2025-01-14 11:03:50 +0530

    change method name

--- src/main/java/com/microservice/review/controller/ReviewController.java
+++ src/main/java/com/microservice/review/controller/ReviewController.java
@@ -26,30 +26,30 @@ public class ReviewController {
     }
 
     @PostMapping
-    public ResponseEntity<String> addJob(@RequestParam Long companyId,@RequestBody Review review) {
+    public ResponseEntity<String> addReview(@RequestParam Long companyId,@RequestBody Review review) {
         if (reviewService.addReview(companyId,review) != null)
             return new ResponseEntity<>("Add Review Succefully", HttpStatus.CREATED);
         return new ResponseEntity<>("Company Id Not Found", HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Review> getJobById(@PathVariable Long id) {
+    public ResponseEntity<Review> getReviewById(@PathVariable Long id) {
         Review review = reviewService.reviewById(id);
         if (review != null)
             return new ResponseEntity<>(review, HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    //@RequestMapping(value = "/jobs/{id}",method =RequestMethod.PUT)
+
     @PutMapping("/{id}")
-    public ResponseEntity<String> UpdateJob(@PathVariable Long id, @RequestBody Review review) {
+    public ResponseEntity<String> UpdateReview(@PathVariable Long id, @RequestBody Review review) {
         if (reviewService.updateReview(id, review))
             return new ResponseEntity<>("Review Updated Succefully", HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteJobById(@PathVariable Long id) {
+    public ResponseEntity<String> deleteReviewById(@PathVariable Long id) {
         if (reviewService.reviewDeleteById(id))
             return new ResponseEntity<>("Review Deleted Succefully", HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
